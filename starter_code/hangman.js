9e8669fe8b9e148ed33e127de25bab8329a545e6
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2017-11-23 17:49:33 +0100

    first

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,30 @@
 var hangman;
-
 function Hangman() {
-
+  this.words = ["perro", "gato", "amsterdam", "murcielago", "conejo"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
 }
-
 Hangman.prototype._getWord = function () {
-
+   return this.words[Math.floor(Math.random() * this.words.length)];
 };
-
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+   return ( keyCode >= 60 && keyCode <=90);
 };
-
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  
 };
-
 Hangman.prototype._addCorrectLetter = function(i){
-
 };
-
 Hangman.prototype._addWrongLetter = function (letter){
-
 };
-
 Hangman.prototype._checkGameOver = function() {
-
 };
-
 Hangman.prototype._checkWinner = function() {
-
 };
-
 document.getElementById("start-game-button").onclick = function(){
   hangman = new Hangman();
 };
-
-
 document.onkeydown = function(e) {
-
 };
